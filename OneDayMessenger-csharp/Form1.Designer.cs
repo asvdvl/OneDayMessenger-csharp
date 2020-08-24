3575a5b365afa07547797bc1757b51caefe9b607
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-08-24 15:22:47 +0300

    Add project files.

---
+++ OneDayMessenger-csharp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace OneDayMessenger_csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.textBoxIMEI = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(96, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(87, 13);
+            label1.TabIndex = 1;
+            label1.Text = "login(40 symbols)";
+            // 
+            // textBoxIMEI
+            // 
+            this.textBoxIMEI.Location = new System.Drawing.Point(12, 29);
+            this.textBoxIMEI.Name = "textBoxIMEI";
+            this.textBoxIMEI.Size = new System.Drawing.Size(251, 20);
+            this.textBoxIMEI.TabIndex = 0;
+            this.textBoxIMEI.Text = "0123456789012345678901234567890123456789";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(99, 95);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(12, 52);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(31, 13);
+            this.labelError.TabIndex = 3;
+            this.labelError.Text = "error:";
+            this.labelError.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 145);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.textBoxIMEI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIMEI;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ OneDayMessenger-csharp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneDayMessenger_csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxIMEI.Text.Length == 40)
+            {
+                
+            }
+            else
+            {
+                labelError.Visible = true;
+                labelError.Text = $"Error: bad length ({textBoxIMEI.Text.Length}/40)";
+            }
+        }
+    }
+}
